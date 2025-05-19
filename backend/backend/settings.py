ebcfbb538bba04cb2f9ab7574a504fd3e349bd1c
Albin Varghese <albin8285@gmail.com>
2025-05-19 15:24:11 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -150,7 +150,12 @@ SIMPLE_JWT = {
 
 # CORS settings (for frontend-backend communication)
 CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS').split(',')
-CSRF_TRUSTED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS').split(',')
+CSRF_TRUSTED_ORIGINS = [
+    "https://crossroadsbackend.marketbytes.in",
+    "https://crossroadscms.marketbytes.in",
+    "http://crossroadscms.marketbytes.in",
+]
+
 
 
 CORS_ALLOW_CREDENTIALS = True
