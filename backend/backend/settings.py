85d31c22796cd1200fd6622146d0a8832123ff93
Albin Varghese <albin8285@gmail.com>
2025-05-19 15:55:11 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -150,7 +150,7 @@ SIMPLE_JWT = {
 
 # CORS settings (for frontend-backend communication)
 CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS').split(',')
-CSRF_TRUSTED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS').split(',')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
 
 
 
